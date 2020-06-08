da40c37ea6819fb90f9e3bcf072d65e271ac4839
tnguyen0317 <thong_nguyen7@hotmail.com>
2020-06-08 16:14:53 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROJECT_ME.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
